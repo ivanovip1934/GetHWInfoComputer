095de8987742ccca0bb38259030769598d17e2d1
ivanovip1934 <ivanovip1934@outlook.com>
2020-03-28 09:48:22 -0700

    Change  class RAMInfo and MonitorInfo

--- GetHWInfoComputer/DiskInfo.cs
+++ GetHWInfoComputer/DiskInfo.cs
@@ -8,34 +8,42 @@ namespace GetHWInfoComputer
 {
     public class DiskInfo
     {
-        public string Model { get;  set; }
-        public int Index { get;  set; }
-        public int Size { get;  set; }
-        public bool IsSSD { get;  set; }
+        public string Model { get; set; }
+        public int Index { get; set; }
+        public int Size { get; set; }
+        public bool IsSSD { get; set; }
         //private List<StorageInfo> Storages { get; set; }
 
-        public DiskInfo(){}
+        public DiskInfo() { }
 
-        public List<DiskInfo> GetStorage() {
+        public List<DiskInfo> GetStorage()
+        {
             List<DiskInfo> storages = new List<DiskInfo>();
             int indexDisk = 0;
             string model = String.Empty;
             bool isSSD;
-            Dictionary<string, string>[] dicProperties= GeneralStaticMethods.GetHardWareInfo<DiskInfo>("Win32_DiskDrive");
-            foreach (Dictionary<string, string> dicprop in dicProperties)
+            Dictionary<string, string>[] dicProperties = GeneralStaticMethods.GetHardWareInfo<DiskInfo>("Win32_DiskDrive");
+            if (dicProperties != null)
             {
-                indexDisk = int.Parse(dicprop["Index"]);
-                model = dicprop["Model"].Replace(" SCSI Disk Device", "").Replace("ATA Device","").Trim();
-                isSSD = CheckStorageIsSSD.HasNominalMediaRotationRate($"\\\\.\\PhysicalDrive{indexDisk}");
-                storages.Add(new DiskInfo
+                foreach (Dictionary<string, string> dicprop in dicProperties)
                 {
-                    Model = model,
-                    Index = indexDisk,
-                    IsSSD = isSSD,
-                    Size = (int)(long.Parse(dicprop["Size"]) / (1024 * 1024 * 1024))
-                });
+                    indexDisk = int.Parse(dicprop["Index"]);
+                    model = dicprop["Model"].Replace(" SCSI Disk Device", "").Replace("ATA Device", "").Trim();
+                    isSSD = CheckStorageIsSSD.HasNominalMediaRotationRate($"\\\\.\\PhysicalDrive{indexDisk}");
+                    storages.Add(new DiskInfo
+                    {
+                        Model = model,
+                        Index = indexDisk,
+                        IsSSD = isSSD,
+                        Size = (int)(long.Parse(dicprop["Size"]) / (1024 * 1024 * 1024))
+                    });
+                }
+                return storages;
             }
-            return storages;
+            else
+            {
+                return null;
+            }
         }
     }
 }
--- GetHWInfoComputer/RAMInfo.cs
+++ GetHWInfoComputer/RAMInfo.cs
@@ -5,28 +5,40 @@ using System.Text;
 
 namespace GetHWInfoComputer
 {
-     [Serializable]
+    [Serializable]
     public class RAMInfo
     {
-        public string Tag { get;  set; }
-        public string PartNumber { get;  set; }
-        public int Capacity { get;  set; }
-       
+        public string Tag { get; set; }
+        public string PartNumber { get; set; }
+        public int Capacity { get; set; }
+
         public RAMInfo()
         {
-           
+
         }
 
         public List<RAMInfo> GetRAM()
         {
             List<RAMInfo> totalRAM = new List<RAMInfo>();
-            Dictionary<string, string>[] dicProperties1 = GeneralStaticMethods.GetHardWareInfo<RAMInfo>("Win32_PhysicalMemory");            
-            foreach (Dictionary<string, string> dicprop in dicProperties1) {
-               totalRAM.Add(new RAMInfo { Tag = dicprop["Tag"],
-                                              PartNumber = dicprop["PartNumber"],
-                                              Capacity = (int)(long.Parse(dicprop["Capacity"])/(1024*1024*1024))});
-             }
-            return totalRAM;            
+            Dictionary<string, string>[] dicProperties1 = GeneralStaticMethods.GetHardWareInfo<RAMInfo>("Win32_PhysicalMemory");
+            if (dicProperties1 != null)
+            {
+
+                foreach (Dictionary<string, string> dicprop in dicProperties1)
+                {
+                    totalRAM.Add(new RAMInfo
+                    {
+                        Tag = dicprop["Tag"],
+                        PartNumber = dicprop["PartNumber"],
+                        Capacity = (int)(long.Parse(dicprop["Capacity"]) / (1024 * 1024 * 1024))
+                    });
+                }
+                return totalRAM;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
